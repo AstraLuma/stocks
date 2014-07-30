ec6bdb971126444957f8c3ef775e7f2eabf0cbe8
Jamie Bliss <astronouth7303@gmail.com>
2014-07-30 19:13:19 -0400

    Most of the work to move to promises

--- javascripts/stocks.js
+++ javascripts/stocks.js
@@ -1,3 +1,89 @@
+var ProJax = {
+	get: function(url, args) {
+		return new Promise(function(resolve, reject) {
+			$.get(url, args).done(resolve).fail(reject);
+		});
+	}
+};
+
+function parseWSJ(data) {
+	var rv = {};
+	$.csv.toArrays(data, {separator: '\t'}).forEach(function(row) {
+		var matches;
+		if (matches = /(.*) \((.*)\)/.exec(row[0])) {
+			rv[matches[2]] = {symbol: matches[2], name: matches[1]};
+		}
+	});
+	return Promise.resolve(rv);
+}
+
+function displayList(sel, data) {
+	var parent = $(sel);
+	for(var sym in data) {
+		var rec = data[sym];
+		parent.append($('<li>').stock(rec.symbol, rec.name));
+	}
+}
+
+function mergeData(datas) {
+	var rv = {};
+	datas.forEach(function(data) {
+		for (var sym in data) {
+			var rec = data[sym];
+			if (rv[sym] == undefined) {
+				rv[sym] = rec; // FIXME: Copy?
+				rv[sym].count = 1;
+			} else {
+				rv[sym].count++;
+			}
+		}
+	});
+	return Promise.resolve(rv);
+}
+
+function getDates(data) {
+	return new Promise(function(resolve, reject) {
+		ProJax.get("http://download.finance.yahoo.com/d/quotes.csv", {
+			s: Object.keys(data),
+			f: 'snr1q0'
+		})
+		.catch(reject)
+		.then(function() {
+			var FORMATS = [
+				"DD-MMM-YY",
+				"MMM DD"
+			];
+
+			$.csv.toArrays(data).forEach(function(row) {
+				if (row.length != 4) {
+					return;
+				}
+				var symbol = row[0];
+				var name = row[1];
+				var divi = row[2];
+				var exdivi = row[3];
+				if (!data[symbol].name) {
+					data[symbol].name = name;
+				}
+				if (divi != '-' && divi != 'N/A') {
+					data[symbol].dividend = moment(divi, FORMATS);
+				}
+				if (exdivi != '-' && exdivi != 'N/A') {
+					data[symbol].exdividend = moment(exdivi, FORMATS);
+				}
+			});
+			resolve(data);
+		});
+	});
+}
+
+function getPortfolio() {
+	if (!localStorage['$portfolio']) 
+		localStorage['$portfolio'] = "[]";
+	return Promise.resolve(JSON.parse(localStorage['$portfolio']));
+}
+
+
 function StockCal(name) {
 	this._name = name;
 	this._items = {};
@@ -80,42 +166,20 @@ var calendars = {
 	wsj: new StockCal("wsj")
 };
 
-function loadWSJ() {
-	$.get("http://online.wsj.com/mdc/public/npage/2_3045-mfgppl-mfxml2csv.html")
-	.done(function(data) {
-		var parent = $('#weaklist');
-		$.csv.toArrays(data, {separator: '\t'}).forEach(function(row) {
-			var matches;
-			if (matches = /(.*) \((.*)\)/.exec(row[0])) {
-				parent.append($('<li>').stock(matches[2], matches[1]));
-				calendars.wsj.add(matches[2]);
-			}
-		});
-		calendars.wsj.finish();
-	})
-	.fail(function (){
-		// TODO
-		alert("Failed to load WSJ data")
-	})
-	;
-}
-
-function loadPortfolio() {
-	if (!localStorage['$portfolio']) localStorage['$portfolio'] = "[]";
-	var syms = JSON.parse(localStorage['$portfolio']);
-	if (syms) {
-		var parent = $('#portfolio');
-		syms.forEach(function(e) {
-			parent.append($('<li>').stock(e));
-			calendars.portfolio.add(e);
-		});
-	}
-	calendars.portfolio.finish();
-}
-
 $(function() {
-	loadWSJ();
-	loadPortfolio();
+	// Make the WSJ data flow
+	var weakness = ProJax.get("http://online.wsj.com/mdc/public/npage/2_3045-mfgppl-mfxml2csv.html").then(parseWSJ),
+		strength = ProJax.get("").then(parseWSJ);
+	weakness.then(function(data) {
+		displayList('#weaklist', data);
+	});
+	strength.then(function(data) {
+		displayList('#stronglist', data);
+	});
+	Promise.all([weakness, strength]).then(mergeData).then(getDates).then(/*Add to calendar*/);
+
+	// Make the Portfolio data flow
+	getPortfolio().then(getDates).then(/*Add to Calendar*/);
 	$('#calendar').fullCalendar({
 		theme: true,
 		header: {
